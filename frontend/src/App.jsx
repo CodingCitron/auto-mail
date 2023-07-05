37aa0636cd25564def50fcaf34f6a4ba3e153c48
sangmin <xectler@gmail.com>
2023-07-06 00:41:00 +0900

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="">
-        red
+      <div className="text-3xl font-bold underline bg-red-200">
+        redasdfsaㄴㄹㄴㅇㅁㄹ
+        asdasda
+        asd
       </div>
     </>
   )
